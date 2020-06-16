6a8ac2cdff2ea1daa629f9da56e3e075d7c9baac
GreenAvocado <greenavocado@protonmail.com>
2020-06-15 20:10:46 -0600

    feat: added api for server-side user verification

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,28 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    console.log(request.body);
-    response.send("Hello from Firebase!");
+const api = express();
+
+const serviceAccount = require('./secret.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
 });
+
+api.post('/api', (req, res) => {
+    console.log(req.body);
+    let idToken = req.body.token;
+    admin.auth().verifyIdToken(idToken)
+        .then(function(decodedToken) {
+            let uid = decodedToken.uid;
+            let email = decodedToken.email;
+
+            return res.status(200).json({ status: 'success', uid: uid, email: email });
+        }).catch(function(error) {
+            return res.status(401).json({ status: 'failure' });
+        });
+});
+
+exports.api = functions.https.onRequest(api);
 
--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ function login() {
         var errorCode = error.code;
         var errorMessage = error.message;
 
-        alert(error.code, error.message);
+        alert(error.code + ' ' + error.message);
     });
 }
 
@@ -18,7 +18,38 @@ function register() {
         var errorCode = error.code;
         var errorMessage = error.message;
 
-        alert(error.code, error.message);
+        alert(error.code + ' ' + error.message);
     });
 }
+
+function secureRequest() {
+    var user = firebase.auth().currentUser;
+
+    if (user) {
+        firebase.auth().currentUser.getIdToken(true).then(function(idToken) {
+            console.log(idToken);
+            postData('/api', { token: idToken })
+                .then(data => {
+                    console.log(data);
+                    alert(data.status + ' ' + data.email + ' ' + data.uid);
+                });
+        });
+    }
+    else {
+        alert('Please log in');
+    }
+}
+
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
+
 
